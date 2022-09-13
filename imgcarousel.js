402fdc1bbc7f45a9a7b37254b383ad5b8a1ca98d
Ryan Yong <81573245+ryanxyong@users.noreply.github.com>
2022-09-12 20:17:05 -0400

    Overhauled website formatting. Added new subpages for projects and for photography pages. Image gallery redone to remove hardcoding and simplify operation

--- imgcarousel.js
+++ imgcarousel.js
@@ -1,47 +1,37 @@
 // Ryan Yong
-// Last updated: 6.15.22
+// Last updated: 9.12.22
 
-// To hold all names of albums and their starting indices
-let albumNames = ["conch", "nature", "architecture", "dartlib", "boots"]
-let currAlbumPic = []
-
-// Initiates current album photo index
-for (albumIdx = 0; albumIdx < albumNames.length; albumIdx++) {
-  currAlbumPic.push(1);
-}
+let picTag = "pic"
+let picIdx = 1
 
 // To go to the next photo
-function nextPic(albumIdx) {
-  photoCarousel(albumIdx, currAlbumPic[albumIdx] += 1);
+function nextPic() {
+  picIdx += 1;
+  photoCarousel();
 }
 
 // To go to the previous photo
-function prevPic(albumIdx) {
-  photoCarousel(albumIdx, currAlbumPic[albumIdx] -= 1);
+function prevPic() {
+  picIdx -= 1
+  photoCarousel();
 }
 
 // Changes the shown photo
-function photoCarousel(albumIdx, currPic) {
+function photoCarousel() {
   let i;
-  var pics = document.getElementsByClassName(albumNames[albumIdx]);
+  var pics = document.getElementsByClassName(picTag);
 
-  if (currPic > pics.length) {currAlbumPic[albumIdx] = 1}
-  if (currPic < 1) {currAlbumPic[albumIdx] = pics.length}
-  currPic = currAlbumPic[albumIdx];
+  if (picIdx > pics.length) {picIdx = 1}
+  if (picIdx < 1) {picIdx = pics.length}
 
   for (i = 0; i < pics.length; i++) {
     pics[i].style.display = "none";
   }
 
-  pics[currPic-1].style.display = "block";
+  pics[picIdx-1].style.display = "block";
 }
 
-// // Runs through to show all base images on page load
-// for (albumIdx = 0; albumIdx < albumNames.length; albumIdx++) {
-//   photoCarousel(albumIdx, currAlbumPic[albumIdx]);
-// }
-
 // Loads initial image
-function loadStart(albumIdx) {
-  photoCarousel(albumIdx, 1);
+function loadStart() {
+  photoCarousel(picTag, picIdx);
 }
