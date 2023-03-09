b7cbf883c2d58b9f357689f286d9bce70a5bbdc1
Ryan Yong <ryanyong@Rye-uhns-MBP.home>
2023-03-09 14:13:52 -0700

    Complete overhaul of personal page with brand new UI and design

---
+++ scripts/home.js
@@ -0,0 +1,34 @@
+let ROOT = "images/home/"
+let bg = document.getElementById('home-bg');
+// let prev_img = "me_bg.JPG"
+// let curr_img = document.documentElement;
+
+document.onmousemove = mouseCoordinates;
+
+function mouseCoordinates(event){
+    let x = event.clientX;
+    let y = event.clientY;
+
+    // let img_name = ""
+
+    if (y < window.innerHeight * .65 && y > window.innerHeight * .32) {
+        if (x < window.innerWidth / 2) {
+            img_name = "swd.jpg"
+        } else {
+            img_name = "photo.jpg"
+        }
+    } else {
+        img_name = "me_bg.JPG"
+    }
+
+    // if (prev_img != img_name) { // restart css animation
+    //     // curr_img.style.setProperty('--img_name', "url(" + ROOT + img_name + ")");
+    //     bg.style.animation = 'none';
+    //     bg.offsetHeight; /* trigger reflow */
+    //     bg.style.animation = null; 
+    // }
+    
+    // prev_img = img_name
+
+    bg.style.backgroundImage = "url(" + ROOT + img_name + ")"
+    }
