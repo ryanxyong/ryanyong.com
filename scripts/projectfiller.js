c6c3e137822a57dfd58411bfbb3cdfe3fc20a268
Ryan Yong <ryanyong@Rye-uhns-MBP.home>
2023-03-13 12:18:41 -0600

    Finished swd portfolio page and added script copy of categoryfiller.js for project portfolio

---
+++ scripts/projectfiller.js
@@ -0,0 +1,11 @@
+const ROOT = "../images/swd/";
+
+let project_div = document.getElementById("project");
+
+let pages = ['../swd/jmmr.html', 'https://github.com/ryanxyong/pong', 'https://github.com/ryanxyong/tinysearchengine', 'https://drive.google.com/drive/folders/1W7GTadLWxzSHAnUb_lbTwP0wrhZeEq5z?usp=sharing', 'https://github.com/ryanxyong/ryanyong.com', 'https://drive.google.com/drive/folders/1A_6VM610AC2T6dmRUh5Iv-8plJxeVICS?usp=sharing', 'https://github.com/ryanxyong/gradientpaste', 'https://drive.google.com/drive/folders/1ed3E-Re82xpIGHbpbIvY5a5evoStnKDT?usp=sharing', 'https://drive.google.com/drive/folders/1bGn2qDDOvBpGLh8rkRHFroxgc7Gw_51n?usp=sharing', 'https://drive.google.com/drive/folders/1TIWlnO7G1yOsvQ02fVP1Zivu13nNDryg?usp=sharing', 'https://github.com/ryanxyong/computerarchitecture']
+
+function addThumbs(num_pages) {
+    for (let i = 0; i < num_pages; i++) {
+        project_div.innerHTML += '<a href="' + pages[i] + '"><img src="' + ROOT + (i+1).toString() + '.jpg" class="clickable photo-thumb"></a>\n';
+    }
+}
