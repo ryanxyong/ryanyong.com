e156224338aed01c434b7d9287491024c9fa9ed8
Ryan Yong <ryanyong@Rye-uhns-MacBook-Pro.local>
2023-05-18 01:30:20 -0400

    Made script go from latest added photos to last added photos

--- scripts/categoryfiller.js
+++ scripts/categoryfiller.js
@@ -3,7 +3,7 @@ const ROOT = "../images/photo/";
 let category_div = document.getElementById("category");
 
 function addThumbs(category, dirs_num) {
-    for (let i = 0; i < dirs_num; i++) {
-        category_div.innerHTML += '<a href="' + category + "/" + (i+1).toString() + '.html"><img src="' + ROOT + category + '/' + (i+1).toString() + '/' + "1" + '.jpg" class="clickable photo-thumb"></a>\n';
+    for (let i = dirs_num; i > 0; i--) {
+        category_div.innerHTML += '<a href="' + category + "/" + (i).toString() + '.html"><img src="' + ROOT + category + '/' + (i).toString() + '/' + "1" + '.jpg" class="clickable photo-thumb"></a>\n';
     }
 }
