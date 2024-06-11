28d81f1e3db2abc0787eae65856e81d6b3e9f142
ryanxyong <ryan.yong.24@dartmouth.edu>
2024-06-10 22:42:03 -0400

    Changed router package and added more compatbility with github pages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import AnimRoutes from './components/AnimRoutes';
 
 //import router
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { Route, HashRouter as Router } from 'react-router-dom';
 
 // import motion
 import { motion } from 'framer-motion';
